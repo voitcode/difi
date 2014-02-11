ce065157f927ddd6e828ced0e3bf2680195c12e6
Runar Ingebrigtsen <runar@rin.no>
2014-02-11 14:38:45 +0100

    Added Norwegian Zip code library

---
+++ lib/posten.rb
@@ -0,0 +1,37 @@
+require 'csv'
+
+# Norwegian zip codes
+class Posten
+  extend Difi
+
+  # The URL to the geo-tagged zip code database by Erik Bolstad
+  def self.url
+    @@url = "http://www.erikbolstad.no/postnummer-koordinatar/txt/postnummer.csv"
+  end
+
+  # Get the zip code database
+  def self.get
+    conn.get url
+  end
+
+  def self.response
+    @@response ||= get
+  end
+
+  # Return zip codes for the given counties
+  def self.zipcodes(*county)
+    @@result = parse.collect {|l| l if county.include?(l[8]) }.compact
+  end
+
+  def self.data
+    @@data ||= response.body
+  end
+
+  def self.lines
+    @@lines ||= data.gsub(/\t/, ',').split("\n")
+  end
+
+  def self.parse
+    @@all_lines ||= lines.collect {|l| l.parse_csv }
+  end
+end
